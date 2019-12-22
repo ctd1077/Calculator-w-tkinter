be610cc50deb8ec15be8781eeb7d62de8ce9f4cd
Cam D <41445364+ctd1077@users.noreply.github.com>
2019-12-22 13:42:50 -0700

    Created subtract,multiply,divide buttons

--- calculator.py
+++ calculator.py
@@ -5,19 +5,12 @@ from tkinter import *
 root = Tk()
 root.title('Simple Calculator')
 e = Entry(root, width=35, borderwidth=5)
-e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
-
-def onClick():
-    hello = 'Hello ' + e.get()
-    myLabel = Label(root, text=hello)
-    myLabel.pack()
+e.grid(row=0, column=0, columnspan=5, padx=10, pady=10)
 
 def onClick(number):
-    #e.delete(0,END)
     current = e.get() # First save the current number displayed
     e.delete(0, END) # Next delete the current number displayed
     e.insert(0, str(current) + str(number)) # Make sure the 
-    # numbers are str's and concat the new number onto the old.
 
 def onClear():
     e.delete(0,END)
@@ -25,13 +18,50 @@ def onClear():
 def onAdd():
     first_num = e.get()
     global f_num
+    global math
+    math = 'addition'
+    f_num = int(first_num)
+    e.delete(0,END)
+
+def onPercent():
+    return
+
+def onSubtract():
+    first_num = e.get()
+    global f_num
+    global math
+    math = 'subtraction'
+    f_num = int(first_num)
+    e.delete(0,END)
+
+def onMultiply():
+    first_num = e.get()
+    global f_num
+    global math
+    math = 'multiplication'
+    f_num = int(first_num)
+    e.delete(0,END)
+
+def onDivide():
+    first_num = e.get()
+    global f_num
+    global math
+    math = 'division'
     f_num = int(first_num)
     e.delete(0,END)
 
 def onEqual():
     second = int(e.get())
     e.delete(0,END)
-    e.insert(0, f_num + second)
+
+    if math == 'addition':
+        e.insert(0, f_num + second)
+    elif math == 'subtraction':
+        e.insert(0, f_num - second)
+    elif math == 'multiplication':
+        e.insert(0, f_num * second)
+    elif math == 'division':
+        e.insert(0, f_num / second)
 
 
 
@@ -47,10 +77,14 @@ button_8 = Button(root, text='8', command=lambda: onClick(8), padx=40, pady=20)
 button_9 = Button(root, text='9', command=lambda: onClick(9), padx=40, pady=20)
 button_0 = Button(root, text='0', command=lambda: onClick(0), padx=40, pady=20)
 
-button_add = Button(root, text='+', command=lambda: onAdd(), padx=40, pady=20)
 button_clear = Button(root, text='C', command=onClear, padx=40, pady=20) # lambda not needed here
 button_equal = Button(root, text='=', command=lambda: onEqual(), padx=90, pady=20)
+button_percent = Button(root, text='/', command=lambda: onPercent(), padx=40, pady=20)
 
+button_add = Button(root, text='+', command=lambda: onAdd(), padx=40, pady=20)
+button_subtract = Button(root, text='-', command=lambda: onSubtract(), padx=40, pady=20)
+button_multiply = Button(root, text='*', command=lambda: onMultiply(), padx=40, pady=20)
+button_divide = Button(root, text='/', command=lambda: onDivide(), padx=40, pady=20)
 
 
 # Add the button on the screen
@@ -66,10 +100,15 @@ button_7.grid(row=1, column=0)
 button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
 
-button_0.grid(row=4, column=1)
-
 button_clear.grid(row=4, column=0)
-button_add.grid(row=4, column=2)
-button_equal.grid(row=5, column=0, columnspan=3)
+button_0.grid(row=4, column=1)
+button_percent.grid(row=4, column=2)
+
+button_divide.grid(row=1, column=3)
+button_multiply.grid(row=2, column=3)
+button_subtract.grid(row=3, column=3)
+button_add.grid(row=4, column=3)
+
+button_equal.grid(row=5, column=0, columnspan=5)
 
 root.mainloop()
